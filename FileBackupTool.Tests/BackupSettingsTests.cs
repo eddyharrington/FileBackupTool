8b5dc8e3d11fceed225cd95756b754f08cf6e5d4
Eddy <eddyharrington@gmail.com>
2018-04-25 18:21:37 -0700

    Added Backup Settings and Tests
    Start backup settings class and proejct changes for unit tests

---
+++ FileBackupTool.Tests/BackupSettingsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileBackupTool;
+
+namespace FileBackupTool.Tests
+{
+    [TestClass]
+    public class BackupSettingsTest
+    {
+        [TestMethod]
+        public void DirectoryExists()
+        {
+            string backupName = "TestBackup";
+            string backupDescription = "This is a test description for the backup.";
+            string sourceDirectory = "C:\\Temp";
+            string destinationDirectory = "C:\\Temp";
+
+            BackupSettings newBackupSettings = new BackupSettings(backupName, backupDescription, sourceDirectory,
+                destinationDirectory);
+            newBackupSettings.ValidateFolderPaths(sourceDirectory);
+            newBackupSettings.ValidateFolderPaths(destinationDirectory);
+        }
+    }
+}
---
+++ FileBackupTool/BackupSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBackupTool
+{
+    public class BackupSettings
+    {
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private string SourceDirectory { get; set; }
+        private string DestinationDirectory { get; set; }
+
+        public BackupSettings(string name, string description, string sourceDirectory, string destinationDirectory)
+        {
+            Name = name;
+            Description = description;
+            SourceDirectory = sourceDirectory;
+            DestinationDirectory = destinationDirectory;
+        }
+
+        public bool ValidateFolderPaths(string folderPath)
+        {
+            //Don't allow null or empty strings as folder paths
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                throw new Exception("The folder path canont be null or empty");
+                //return false;
+            }
+            //Don't allow non-existant folder paths
+            else if (!Directory.Exists(folderPath))
+            {
+                throw new Exception("The folder path does not exist");
+                //return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
