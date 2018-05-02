f8e4cf5f192abd66693c3bd0b7955445795c11bb
Eddy <eddyharrington@gmail.com>
2018-05-01 22:34:11 -0700

    Added UI/UX for selecting backup folders
    Added UI for backup name/description/selected directors, as well as UX for folder dialog browsing

--- FileBackupTool/FormNewBackup.Designer.cs
+++ FileBackupTool/FormNewBackup.Designer.cs
@@ -40,12 +40,17 @@
             this.radioButtonSelectFiles = new System.Windows.Forms.RadioButton();
             this.lblBackupFromValidate = new System.Windows.Forms.Label();
             this.lblBackupToValidate = new System.Windows.Forms.Label();
+            this.lbBackupDirectories = new System.Windows.Forms.ListBox();
+            this.txtBoxBackupDescription = new System.Windows.Forms.TextBox();
+            this.txtBoxBackupName = new System.Windows.Forms.TextBox();
+            this.lblBackupName = new System.Windows.Forms.Label();
+            this.lblBackupDescription = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblBackupFrom
             // 
             this.lblBackupFrom.AutoSize = true;
-            this.lblBackupFrom.Location = new System.Drawing.Point(11, 15);
+            this.lblBackupFrom.Location = new System.Drawing.Point(11, 81);
             this.lblBackupFrom.Name = "lblBackupFrom";
             this.lblBackupFrom.Size = new System.Drawing.Size(70, 13);
             this.lblBackupFrom.TabIndex = 0;
@@ -54,7 +59,7 @@
             // lblBackupTo
             // 
             this.lblBackupTo.AutoSize = true;
-            this.lblBackupTo.Location = new System.Drawing.Point(22, 41);
+            this.lblBackupTo.Location = new System.Drawing.Point(22, 107);
             this.lblBackupTo.Name = "lblBackupTo";
             this.lblBackupTo.Size = new System.Drawing.Size(59, 13);
             this.lblBackupTo.TabIndex = 1;
@@ -62,35 +67,37 @@
             // 
             // txtBoxBackupFrom
             // 
-            this.txtBoxBackupFrom.Location = new System.Drawing.Point(87, 12);
+            this.txtBoxBackupFrom.Location = new System.Drawing.Point(87, 78);
             this.txtBoxBackupFrom.Name = "txtBoxBackupFrom";
             this.txtBoxBackupFrom.Size = new System.Drawing.Size(185, 20);
             this.txtBoxBackupFrom.TabIndex = 2;
             // 
             // txtBoxBackupTo
             // 
-            this.txtBoxBackupTo.Location = new System.Drawing.Point(87, 38);
+            this.txtBoxBackupTo.Location = new System.Drawing.Point(87, 104);
             this.txtBoxBackupTo.Name = "txtBoxBackupTo";
             this.txtBoxBackupTo.Size = new System.Drawing.Size(185, 20);
             this.txtBoxBackupTo.TabIndex = 3;
             // 
             // btnBackupFrom
             // 
-            this.btnBackupFrom.Location = new System.Drawing.Point(291, 10);
+            this.btnBackupFrom.Location = new System.Drawing.Point(291, 76);
             this.btnBackupFrom.Name = "btnBackupFrom";
             this.btnBackupFrom.Size = new System.Drawing.Size(75, 23);
             this.btnBackupFrom.TabIndex = 4;
             this.btnBackupFrom.Text = "Browse...";
             this.btnBackupFrom.UseVisualStyleBackColor = true;
+            this.btnBackupFrom.Click += new System.EventHandler(this.btnBackupFrom_Click);
             // 
             // btnBackupTo
             // 
-            this.btnBackupTo.Location = new System.Drawing.Point(291, 36);
+            this.btnBackupTo.Location = new System.Drawing.Point(291, 102);
             this.btnBackupTo.Name = "btnBackupTo";
             this.btnBackupTo.Size = new System.Drawing.Size(75, 23);
             this.btnBackupTo.TabIndex = 5;
             this.btnBackupTo.Text = "Browse...";
             this.btnBackupTo.UseVisualStyleBackColor = true;
+            this.btnBackupTo.Click += new System.EventHandler(this.btnBackupTo_Click);
             // 
             // btnStartBackup
             // 
@@ -99,17 +106,18 @@
             this.btnStartBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStartBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStartBackup.ForeColor = System.Drawing.Color.White;
-            this.btnStartBackup.Location = new System.Drawing.Point(291, 64);
+            this.btnStartBackup.Location = new System.Drawing.Point(291, 130);
             this.btnStartBackup.Name = "btnStartBackup";
             this.btnStartBackup.Size = new System.Drawing.Size(75, 23);
             this.btnStartBackup.TabIndex = 7;
             this.btnStartBackup.Text = "Backup!";
             this.btnStartBackup.UseVisualStyleBackColor = false;
+            this.btnStartBackup.Click += new System.EventHandler(this.btnStartBackup_Click);
             // 
             // lblFiles
             // 
             this.lblFiles.AutoSize = true;
-            this.lblFiles.Location = new System.Drawing.Point(50, 64);
+            this.lblFiles.Location = new System.Drawing.Point(50, 130);
             this.lblFiles.Name = "lblFiles";
             this.lblFiles.Size = new System.Drawing.Size(31, 13);
             this.lblFiles.TabIndex = 11;
@@ -118,7 +126,7 @@
             // radioButtonEverything
             // 
             this.radioButtonEverything.AutoSize = true;
-            this.radioButtonEverything.Location = new System.Drawing.Point(87, 64);
+            this.radioButtonEverything.Location = new System.Drawing.Point(87, 130);
             this.radioButtonEverything.Name = "radioButtonEverything";
             this.radioButtonEverything.Size = new System.Drawing.Size(75, 17);
             this.radioButtonEverything.TabIndex = 12;
@@ -129,7 +137,7 @@
             // radioButtonSelectFiles
             // 
             this.radioButtonSelectFiles.AutoSize = true;
-            this.radioButtonSelectFiles.Location = new System.Drawing.Point(180, 64);
+            this.radioButtonSelectFiles.Location = new System.Drawing.Point(180, 130);
             this.radioButtonSelectFiles.Name = "radioButtonSelectFiles";
             this.radioButtonSelectFiles.Size = new System.Drawing.Size(79, 17);
             this.radioButtonSelectFiles.TabIndex = 13;
@@ -140,7 +148,7 @@
             // lblBackupFromValidate
             // 
             this.lblBackupFromValidate.AutoSize = true;
-            this.lblBackupFromValidate.Location = new System.Drawing.Point(274, 15);
+            this.lblBackupFromValidate.Location = new System.Drawing.Point(274, 81);
             this.lblBackupFromValidate.Name = "lblBackupFromValidate";
             this.lblBackupFromValidate.Size = new System.Drawing.Size(0, 13);
             this.lblBackupFromValidate.TabIndex = 14;
@@ -148,16 +156,61 @@
             // lblBackupToValidate
             // 
             this.lblBackupToValidate.AutoSize = true;
-            this.lblBackupToValidate.Location = new System.Drawing.Point(274, 41);
+            this.lblBackupToValidate.Location = new System.Drawing.Point(274, 107);
             this.lblBackupToValidate.Name = "lblBackupToValidate";
             this.lblBackupToValidate.Size = new System.Drawing.Size(0, 13);
             this.lblBackupToValidate.TabIndex = 15;
+            // 
+            // lbBackupDirectories
+            // 
+            this.lbBackupDirectories.FormattingEnabled = true;
+            this.lbBackupDirectories.Location = new System.Drawing.Point(14, 165);
+            this.lbBackupDirectories.Name = "lbBackupDirectories";
+            this.lbBackupDirectories.Size = new System.Drawing.Size(352, 95);
+            this.lbBackupDirectories.TabIndex = 16;
+            // 
+            // txtBoxBackupDescription
+            // 
+            this.txtBoxBackupDescription.Location = new System.Drawing.Point(87, 52);
+            this.txtBoxBackupDescription.Name = "txtBoxBackupDescription";
+            this.txtBoxBackupDescription.Size = new System.Drawing.Size(185, 20);
+            this.txtBoxBackupDescription.TabIndex = 20;
+            // 
+            // txtBoxBackupName
+            // 
+            this.txtBoxBackupName.Location = new System.Drawing.Point(87, 26);
+            this.txtBoxBackupName.Name = "txtBoxBackupName";
+            this.txtBoxBackupName.Size = new System.Drawing.Size(185, 20);
+            this.txtBoxBackupName.TabIndex = 19;
+            // 
+            // lblBackupName
+            // 
+            this.lblBackupName.AutoSize = true;
+            this.lblBackupName.Location = new System.Drawing.Point(11, 29);
+            this.lblBackupName.Name = "lblBackupName";
+            this.lblBackupName.Size = new System.Drawing.Size(76, 13);
+            this.lblBackupName.TabIndex = 21;
+            this.lblBackupName.Text = "Backup name:";
+            // 
+            // lblBackupDescription
+            // 
+            this.lblBackupDescription.AutoSize = true;
+            this.lblBackupDescription.Location = new System.Drawing.Point(11, 55);
+            this.lblBackupDescription.Name = "lblBackupDescription";
+            this.lblBackupDescription.Size = new System.Drawing.Size(63, 13);
+            this.lblBackupDescription.TabIndex = 22;
+            this.lblBackupDescription.Text = "Description:";
             // 
             // FormNewBackup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(386, 105);
+            this.ClientSize = new System.Drawing.Size(386, 325);
+            this.Controls.Add(this.lblBackupDescription);
+            this.Controls.Add(this.lblBackupName);
+            this.Controls.Add(this.txtBoxBackupDescription);
+            this.Controls.Add(this.txtBoxBackupName);
+            this.Controls.Add(this.lbBackupDirectories);
             this.Controls.Add(this.lblBackupToValidate);
             this.Controls.Add(this.lblBackupFromValidate);
             this.Controls.Add(this.radioButtonSelectFiles);
@@ -193,6 +246,11 @@
         private System.Windows.Forms.RadioButton radioButtonSelectFiles;
         private System.Windows.Forms.Label lblBackupFromValidate;
         private System.Windows.Forms.Label lblBackupToValidate;
+        private System.Windows.Forms.ListBox lbBackupDirectories;
+        private System.Windows.Forms.TextBox txtBoxBackupDescription;
+        private System.Windows.Forms.TextBox txtBoxBackupName;
+        private System.Windows.Forms.Label lblBackupName;
+        private System.Windows.Forms.Label lblBackupDescription;
     }
 }
 
--- FileBackupTool/FormNewBackup.cs
+++ FileBackupTool/FormNewBackup.cs
@@ -16,5 +16,43 @@ namespace FileBackupTool
         {
             InitializeComponent();
         }
+
+        private void btnBackupFrom_Click(object sender, EventArgs e)
+        {
+            //Open folder browser dialog and let user choose the SOURCE folder they want to backup from
+            string folderPath = "";
+            FolderBrowserDialog dialog = new FolderBrowserDialog(); //TODO file extensions to exclude
+            dialog.Description = "Select the source folder";
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                folderPath = dialog.SelectedPath;
+                txtBoxBackupFrom.Text = dialog.SelectedPath;
+                lbBackupDirectories.Items.Add(folderPath);
+            }
+        }
+
+        private void btnBackupTo_Click(object sender, EventArgs e)
+        {
+            //Open folder browser dialog and let user choose the DESTINATION folder they want to backup to
+            string folderPath = "";
+            FolderBrowserDialog dialog = new FolderBrowserDialog(); //TODO file extensions to exclude
+            dialog.Description = "Select the destination folder";
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                folderPath = dialog.SelectedPath;
+                txtBoxBackupTo.Text = dialog.SelectedPath;
+            }
+
+        }
+
+        private void btnStartBackup_Click(object sender, EventArgs e)
+        {
+            BackupSettings bs = new BackupSettings(txtBoxBackupName.Text, txtBoxBackupDescription.Text,
+    txtBoxBackupFrom.Text, txtBoxBackupTo.Text);
+        }
     }
 }
